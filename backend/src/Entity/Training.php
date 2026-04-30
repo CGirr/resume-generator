c27c46eedef5b0c18aec7d02f3f13a5e89f19bba
Coraline Girr <coraline.girr@gmail.com>
2026-04-30 21:38:54 +0200

    feat(api): initialize CV model with Resume, Experience, Training, Tag and Language entities + API Platform setup

--- backend/src/Entity/Training.php
+++ backend/src/Entity/Training.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TrainingRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TrainingRepository::class)]
+#[ApiResource]
 class Training
 {
     #[ORM\Id]
